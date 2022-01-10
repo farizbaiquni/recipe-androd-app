9ac3ea6c418aabced5ffd516db3b6d5cfb19b636
Fariz Baiquni <farizbaiquni@gmail.com>
2022-01-10 18:58:11 +0700

    add file that respresented recipe

--- app/src/main/java/com/example/recipe_app_jetpack_compose/network/RecipeNetworkEntity.kt
+++ app/src/main/java/com/example/recipe_app_jetpack_compose/network/RecipeNetworkEntity.kt
@@ -1,7 +1,46 @@
 package com.example.recipe_app_jetpack_compose.network
 
+import com.google.gson.annotations.SerializedName
+
 
 class RecipeNetworkEntity {
 
+    @SerializedName("pk")
+    var pk: Int? = null
 
+    @SerializedName("title")
+    var title: String? = null
+
+    @SerializedName("publisher")
+    var publisher: String? = null
+
+    @SerializedName("featured_image")
+    var featuredImage: String? = null
+
+    @SerializedName("rating")
+    var rating: Int? = null
+
+    @SerializedName("source_url")
+    var sourceUrl: String? = null
+
+    @SerializedName("description")
+    var description: String? = null
+
+    @SerializedName("cooking_instructions")
+    var cookingInstructions: String? = null
+
+    @SerializedName("ingredients")
+    var ingredients: List<String>? = null
+
+    @SerializedName("date_added")
+    var dateAdded: String? = null
+
+    @SerializedName("date_updated")
+    var dateUpdated: String? = null
+
+    @SerializedName("long_date_added")
+    var longDateAdded: Long? = null
+
+    @SerializedName("long_date_updated")
+    var longDateUpdated: Long? = null
 }
